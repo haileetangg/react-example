b2e63676dace6b2352335ff8322f32f12a9f3ac6
haileetangg <153844571+haileetangg@users.noreply.github.com>
2024-02-06 22:07:49 +0800

    提交学习react的代码

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider theme={{token: { colorPrimary: 'hotpink' }}}>
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
